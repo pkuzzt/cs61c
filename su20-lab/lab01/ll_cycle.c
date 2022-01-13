0d108ead223d92c253331adfc87c443016bd5b9c
zhitao zhu <zhuzt@pku.edu.cn>
2022-01-13 16:43:43 +0800

    complete lab01

--- su20-lab/lab01/ll_cycle.c
+++ su20-lab/lab01/ll_cycle.c
@@ -3,5 +3,18 @@
 
 int ll_has_cycle(node *head) {
     /* your code here */
+    struct node *hare, *tortoise;
+    hare = head;
+    tortoise = head;
+    while (hare && tortoise) {
+        tortoise = tortoise->next;
+        hare = hare->next;
+        if (hare == NULL)
+            break;
+        hare = hare->next;
+        if (tortoise == hare) {
+            return 1;
+        }
+    }
     return 0;
 }
