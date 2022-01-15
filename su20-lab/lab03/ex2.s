26197d0380ba3c623da11bd617565441dfc69226
zhitao zhu <zhuzt@pku.edu.cn>
2022-01-16 00:51:04 +0800

    complete lab03

--- su20-lab/lab03/ex2.s
+++ su20-lab/lab03/ex2.s
@@ -26,23 +26,26 @@ main:
     la s1, source
     la s2, dest
 loop:
-    slli s3, t0, 2
-    add t1, s1, s3
-    lw t2, 0(t1)
-    beq t2, x0, exit
-    add a0, x0, t2
+    slli s3, t0, 2 # t0 = k
+    add t1, s1, s3 # t1 = source + t0  
+    lw t2, 0(t1) # t2 = source[k]
+    beq t2, x0, exit # if (source[k] == 0) exit;
+
+    add a0, x0, t2 # a0 = source[k];
+
     addi sp, sp, -8
     sw t0, 0(sp)
     sw t2, 4(sp)
     jal square
     lw t0, 0(sp)
     lw t2, 4(sp)
-    addi sp, sp, 8
-    add t2, x0, a0
-    add t3, s2, s3
-    sw t2, 0(t3)
-    add s0, s0, t2
-    addi t0, t0, 1
+    addi sp, sp, 8 # a0 = func(a0);
+
+    add t2, x0, a0 # t2 = a0
+    add t3, s2, s3 # t3 = source + t0
+    sw t2, 0(t3) 
+    add s0, s0, t2 # s0 = sum, sum = sum + func(source[k]);
+    addi t0, t0, 1 # t0 = k;
     jal x0, loop
 square:
     add t0, a0, x0
@@ -52,6 +55,7 @@ square:
     mul t1, t1, t2
     mul a0, t0, t1
     jr ra
+    
 exit:
     add a0, x0, s0
     add a1, x0, x0
--- su20-lab/lab03/factorial.s
+++ su20-lab/lab03/factorial.s
@@ -6,11 +6,11 @@ n: .word 8
 .text
 main:
     la t0, n
-    lw a0, 0(t0)
+    lw a0, 0(t0) # a0 = n
     jal ra, factorial
 
-    addi a1, a0, 0
-    addi a0, x0, 1
+    addi a1, a0, 0 # a1 = factorial(n)
+    addi a0, x0, 1 # a0 = 1
     ecall # Print Result
 
     addi a1, x0, '\n'
@@ -22,3 +22,12 @@ main:
 
 factorial:
     # YOUR CODE HERE
+    addi t0, x0, 1
+    addi t1, x0, 1
+loop:
+    mul t1, t1, t0
+    addi t0, t0, 1
+    bgeu a0, t0, loop
+    addi a0, t1, 0
+    jr ra
+
