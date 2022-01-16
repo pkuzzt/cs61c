9cae8f766c814345b82436a88f7faa598fe7f21d
zhitao zhu <zhuzt@pku.edu.cn>
2022-01-16 16:30:37 +0800

    complete proj2-task3.1

--- su20-proj2/src/dot.s
+++ su20-proj2/src/dot.s
@@ -18,27 +18,49 @@
 # this function exits with error code 6.
 # =======================================================
 dot:
-
-    # Prologue
-
-
+    bge x0, a2, dot_exit1
+    bge x0, a3, dot_exit2
+    bge x0, a4, dot_exit2
+    addi sp, sp, -16
+    sw s0, 0(sp)
+    sw s1, 4(sp)
+    sw s2, 8(sp)
+    sw s3, 12(sp) 
+    mv t0, a0
+    mv t1, a1
+    mv t2, a2
+    mv s0, a3
+    mv s1, a4
+    li t3, 0
+    li t4, 0
 loop_start:
-
-
-
-
-
-
-
-
-
-
-
-
+    mul s2, t3, s0
+    mul s3, t3, s1
+    slli s2, s2, 2
+    slli s3, s3, 2
+    add s2, s2, t0
+    add s3, s3, t0
+    lw s2, 0(s2) # s2 = v0[i * step1]
+    lw s3, 0(s3) # s3 = v1[i * step2]
+    mul s2, s2, s3 # s2 = v0[i * step1] * v1[i * step1]
+    add t4, t4, s2 # t4 = sum
+    addi t3, t3, 1
+    bge t3, a2, loop_end
+    jal x0, loop_start
 loop_end:
-
-
-    # Epilogue
-
-    
+    lw s0, 0(sp)
+    lw s1, 4(sp)
+    lw s2, 8(sp)
+    lw s3, 12(sp)
+    lw s4, 16(sp)
+    addi sp, sp, 16
+    mv a0, t4
     ret
+
+dot_exit1:
+    li a1, 5
+    jal exit2 
+
+dot_exit2:
+    li a1, 6
+    jal exit2
--- su20-proj2/tests/assembly/test_dot.s
+++ su20-proj2/tests/assembly/test_dot.s
@@ -15,20 +15,21 @@ main:
     la s1 vector1
 
     # Set vector attributes
-    
-
-
+    mv a0, s0
+    mv a1, s1
+    li a2, 3 # a2 = len
+    li a3, 1 # a3 = step1
+    li a4, 2 # a4 = step2
     # Call dot function
-    
-
+    jal ra dot
 
     # Print integer result
-
-
+    mv a1 a0
+    jal ra print_int
 
     # Print newline
-
-
+    li a1 '\n'
+    jal ra print_char
 
     # Exit
     jal exit
