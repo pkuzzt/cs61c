97df87b6c970a633d848777d9e64bb0d31e03f3a
zhu zhitao <zhuzt@pku.edu.cn>
2022-01-17 16:17:52 +0800

    complete proj2

--- su20-proj2/tests/run.sh
+++ su20-proj2/tests/run.sh
@@ -1,3 +1,3 @@
-java -jar ../tools/venus.jar assembly/test_write_matrix.s
-python3 ../tools/convert.py --to-ascii outputs/test_write_matrix/student_write_outputs.bin output.txt 
+java -jar ../tools/venus.jar ../src/main.s -ms -1 ./inputs/mnist/bin/m0.bin ./inputs/mnist/bin/m1.bin ./inputs/mnist/bin/inputs/mnist_input0.bin ./outputs/test_mnist_main/student_mnist_output.bin
+python3 ../tools/convert.py --to-ascii ./outputs/test_mnist_main/student_mnist_output.bin output.txt
 cat output.txt
